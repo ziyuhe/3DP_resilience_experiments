6ab13391972acecaed76325635b4884d538960e0
Ziyu He <ziyu.he.kenyon@gmail.com>
2025-03-24 17:08:12 -0700

    Update BoE_Approx_Max_Submod_SAA_alternative.m

--- Utilities/BoE_Approx_Max_Submod_SAA_alternative.m
+++ Utilities/BoE_Approx_Max_Submod_SAA_alternative.m
@@ -2,7 +2,6 @@ function output = BoE_Approx_Max_Submod_SAA_alternative(input)
 
 % =========================================================================
 % Script Name:      BoE_Approx_Max_Submod_SAA_alternative.m
-% Author:            Ziyu He
 % Date:              02/01/2025
 % Description:       
 %   - Implements a **local search algorithm** for **supermodular approximation (BOE)** 
